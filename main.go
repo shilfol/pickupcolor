b156bb180482e8dcb17d48c7270539d2873a542f
shilfol <yayananoda@gmail.com>
2017-12-09 02:48:42 +0900

    機能の実装完了 残修正点有
    初期位置を決めkmeans++にする

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
 	_ "image/jpeg"
-	_ "image/png"
+	"image/png"
 	"log"
 	"math/rand"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/lucasb-eyer/go-colorful"
 )
@@ -48,28 +51,65 @@ func main() {
 			}
 		}
 	}
+	rand.Seed(time.Now().UnixNano())
 	results := kmeans(imageVectors)
 
-	fmt.Println(results)
+	for i, result := range results {
+		fmt.Println(i, result.vector.Hex())
+	}
+	outimage := image.NewRGBA(image.Rect(0, 0, 960, 320))
+	for x := 0; x < 960; x++ {
+		for y := 0; y < 320; y++ {
+			vec := results[int(x/160)].vector
+			r, g, b := vec.RGB255()
+			c := color.RGBA{r, g, b, 255}
+			outimage.Set(x, y, c)
+		}
+	}
+
+	filename := strings.Split(filepath, ".")
+	if len(filename) < 1 {
+		return
+	}
+	newfilename := filename[0] + "-pickupcolor.png"
+	f, err = os.OpenFile(newfilename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer f.Close()
+	png.Encode(f, outimage)
 }
 
 func kmeans(vectors []imageVector) []imageVector {
-	resultVector := []imageVector{}
+	resultVectors := []imageVector{}
 
+	//FIXME kmeans++ は初期ベクトルを均等に散らす必要がある
 	for i := 1; i <= 6; i++ {
 		tmpVector := imageVector{colorful.Color{rand.Float64(), rand.Float64(), rand.Float64()}, i}
-		resultVector = append(resultVector, tmpVector)
+		resultVectors = append(resultVectors, tmpVector)
 	}
 
-	for _, vector := range vectors {
-		vector.group = detectGroup(vector, resultVector)
+	for i := 0; i < len(vectors); i++ {
+		vectors[i].group = detectGroup(vectors[i], resultVectors)
 	}
 
-	//TODO resultVectorの再設定
+	copyVectors := make([]imageVector, len(resultVectors))
+	for {
+		copy(copyVectors, resultVectors)
+		for i := 0; i < len(resultVectors); i++ {
+			resultVectors[i] = resetCenterVector(vectors, resultVectors[i])
+		}
+		if checkEqual(copyVectors, resultVectors) {
+			break
+		}
 
-	//TODO imagevectorsの再クラスタリング
+		for i := 0; i < len(vectors); i++ {
+			vectors[i].group = detectGroup(vectors[i], resultVectors)
+		}
+	}
 
-	return resultVector
+	return resultVectors
 }
 
 func detectGroup(vector imageVector, clusters []imageVector) int {
@@ -87,4 +127,41 @@ func detectGroup(vector imageVector, clusters []imageVector) int {
 		}
 	}
 	return group
+}
+
+func resetCenterVector(vectors []imageVector, cluster imageVector) imageVector {
+	newVector := imageVector{}
+	newVector.group = cluster.group
+	count := 0.0
+	for _, vector := range vectors {
+		if cluster.group == vector.group {
+			newVector.vector.R += vector.vector.R
+			newVector.vector.G += vector.vector.G
+			newVector.vector.B += vector.vector.B
+			count += 1.0
+		}
+	}
+
+	if count != 0.0 {
+		newVector.vector.R /= count
+		newVector.vector.G /= count
+		newVector.vector.B /= count
+	} else {
+		fmt.Println("no members")
+	}
+
+	return newVector
+
+}
+
+func checkEqual(prev []imageVector, after []imageVector) bool {
+	if len(prev) != len(after) {
+		return false
+	}
+	for i := 0; i < len(prev); i++ {
+		if prev[i].vector.R != after[i].vector.R || prev[i].vector.G != after[i].vector.G || prev[i].vector.B != after[i].vector.B || prev[i].group != after[i].group {
+			return false
+		}
+	}
+	return true
 }
